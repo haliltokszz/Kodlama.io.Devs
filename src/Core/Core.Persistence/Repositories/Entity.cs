b8e0e6c95c5cd67b626c3824f234a9686ba382ad
haliltokszz <toksozhalil@gmail.com>
2022-09-06 16:59:50 +0300

    feature: base entity model added.

---
+++ src/Core/Core.Persistence/Repositories/Entity.cs
@@ -0,0 +1,20 @@
+﻿namespace Core.Persistence.Repositories;
+
+public class Entity
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public DateTime CreatedDate { get; }
+    public DateTime UpdatedDate { get; set; }
+    public DateTime DeletedDate { get; }
+    public bool isDeleted { get; set; } = false;
+
+    public Entity()
+    {
+        CreatedDate = DateTime.UtcNow;
+    }
+    
+    public Entity(Guid id) : this()
+    {
+        Id = id;
+    }
+}
