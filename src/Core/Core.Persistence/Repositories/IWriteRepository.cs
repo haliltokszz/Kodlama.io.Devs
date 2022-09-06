cf5975d93934d8de3115a6f688a886749ce97918
haliltokszz <toksozhalil@gmail.com>
2022-09-06 22:19:56 +0300

    refactor: return types changed. feature: added SaveChangesAsync method to all functions optionally.

--- src/Core/Core.Persistence/Repositories/IWriteRepository.cs
+++ src/Core/Core.Persistence/Repositories/IWriteRepository.cs
@@ -2,12 +2,11 @@
 
 public interface IWriteRepository<T> : IRepository<T> where T : Entity
 {
-    Task<bool> AddAsync(T model);
-    Task<bool> AddRangeAsync(List<T> datas);
-    bool Remove(T model);
-    bool RemoveRange(List<T> datas);
-    Task<bool> RemoveAsync(string id);
-    bool Update(T model);
-
+    Task<T> AddAsync(T entity, bool withSave = true);
+    Task<List<T>> AddRangeAsync(List<T> entities, bool withSave = true);
+    Task<T> Remove(T entity, bool withSave = true);
+    Task<List<T>> RemoveRange(List<T> entities, bool withSave = true);
+    Task<T> RemoveAsync(string id, bool withSave = true);
+    Task<T> Update(T entity, bool withSave = true);
     Task<int> SaveAsync();
 }
