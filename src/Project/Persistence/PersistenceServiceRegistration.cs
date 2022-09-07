80b85ed9c9e0fd0991781e86c552cfc7398e0d58
haliltokszz <toksozhalil@gmail.com>
2022-09-07 15:07:58 +0300

    feature: PersistenceServiceRegistration.cs added.

---
+++ src/Project/Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Application.Services.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+using Persistence.Repositories;
+
+namespace Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+                                                                IConfiguration configuration)
+        {
+            services.AddDbContext<KodlamaIODevsDbContext>(options =>
+                                                     options.UseSqlServer(
+                                                         configuration.GetConnectionString("KodlamaIODevsConnectionString")));
+            services.AddScoped<IProgrammingLanguageWriteRepository, ProgrammingLanguageWriteRepository>();
+            services.AddScoped<IProgrammingLanguageReadRepository, ProgrammingLanguageReadRepository>();
+
+            return services;
+        }
+    }
+}
