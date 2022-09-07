5c8a8581fc8674d3c64d014f1e9bbc73ea88c5e1
haliltokszz <toksozhalil@gmail.com>
2022-09-07 15:11:34 +0300

    feature: MappingProfile.cs added for Programming Language entity.

---
+++ src/Project/Application/Features/ProgrammingLanguages/Profiles/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using Application.Features.ProgrammingLanguages.Commands;
+using Application.Features.ProgrammingLanguages.DTOs;
+using Application.Features.ProgrammingLanguages.Models;
+using AutoMapper;
+using Core.Persistence.Paging;
+using Domain.Entities;
+
+namespace Application.Features.ProgrammingLanguages.Profiles;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<ProgrammingLanguage, CreatedProgrammingLanguageDTO>().ReverseMap();
+        CreateMap<ProgrammingLanguage, CreateProgrammingLanguageCommand>().ReverseMap();
+        CreateMap<ProgrammingLanguage, ProgrammingLanguageListDTO>().ReverseMap();
+        CreateMap<ProgrammingLanguage, ProgrammingLanguageByIdDTO>().ReverseMap();
+        CreateMap<IPaginate<ProgrammingLanguage>, ProgrammingLanguageListModel>().ReverseMap();
+    }
+}
