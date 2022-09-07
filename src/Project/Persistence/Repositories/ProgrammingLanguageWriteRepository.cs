733513a047c7a34b3da9e87e00f42490b5a371ff
haliltokszz <toksozhalil@gmail.com>
2022-09-07 15:05:35 +0300

    feature: ProgrammingLanguageWriteRepository.cs added.

---
+++ src/Project/Persistence/Repositories/ProgrammingLanguageWriteRepository.cs
@@ -0,0 +1,13 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Persistence.Contexts;
+
+namespace Persistence.Repositories;
+
+public class ProgrammingLanguageWriteRepository : WriteRepository<ProgrammingLanguage, KodlamaIODevsDbContext>, IProgrammingLanguageWriteRepository
+{
+    public ProgrammingLanguageWriteRepository(KodlamaIODevsDbContext context) : base(context)
+    {
+    }
+}
