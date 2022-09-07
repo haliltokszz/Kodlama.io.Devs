5c2584f9bf538386ec0c2d195dad7d5ee24e7e07
haliltokszz <toksozhalil@gmail.com>
2022-09-07 15:07:42 +0300

    feature: DesignTimeDbContextFactory.cs factory class added for use both Dotnet.CLI and Package Manager.

---
+++ src/Project/Persistence/DesignTimeDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Persistence.Contexts;
+
+namespace Persistence;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<KodlamaIODevsDbContext>
+{
+    public KodlamaIODevsDbContext CreateDbContext(string[] args)
+    {
+        ConfigurationManager configManager = new ConfigurationManager();
+        configManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../WebAPI"));
+        configManager.AddJsonFile("appsettings.json");
+        
+        var optionsBuilder = new DbContextOptionsBuilder<KodlamaIODevsDbContext>();
+        optionsBuilder.UseSqlServer(configManager.GetConnectionString("KodlamaIODevsConnectionString"));
+
+        return new(optionsBuilder.Options);
+    }
+}
