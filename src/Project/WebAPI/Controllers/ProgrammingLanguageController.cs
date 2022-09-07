46324a18b73346e556597689c7dcbd7622865dde
haliltokszz <toksozhalil@gmail.com>
2022-09-07 15:08:58 +0300

    feature: ProgrammingLanguageController.cs added.

---
+++ src/Project/WebAPI/Controllers/ProgrammingLanguageController.cs
@@ -0,0 +1,32 @@
+﻿using Application.Features.ProgrammingLanguages.Commands;
+using Application.Features.ProgrammingLanguages.Queries;
+using Core.Application.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProgrammingLanguageController : BaseController
+{
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] CreateProgrammingLanguageCommand createProgrammingLanguageCommand)
+    {
+        var result = await Mediator.Send(createProgrammingLanguageCommand);
+        return Created("",result);
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+    {
+        var result = await Mediator.Send(new GetListProgrammingLanguageQuery() { PageRequest = pageRequest });
+        return Ok(result);
+    }
+    
+    [HttpGet("{Id}")]
+    public async Task<IActionResult> Get([FromRoute] GetByIdProgrammingLanguageQuery getByIdProgrammingLanguageQuery)
+    {
+        var result = await Mediator.Send(getByIdProgrammingLanguageQuery);
+        return Ok(result);
+    }
+}
