dfc98ef9e37c84dd7bb67b71eec09477a67448ab
haliltokszz <toksozhalil@gmail.com>
2022-09-07 17:06:27 +0300

    feature: SaveChange interceptor added.

--- src/Project/Persistence/Contexts/KodlamaIODevsDbContext.cs
+++ src/Project/Persistence/Contexts/KodlamaIODevsDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Persistence.Dynamic;
+using Core.Persistence.Repositories;
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,5 +20,21 @@ public class KodlamaIODevsDbContext : DbContext
         
         ProgrammingLanguage[] programmingLanguagesSeeds = { new(Guid.NewGuid(), "C#"), new(Guid.NewGuid(), "JavaScript"), new(Guid.NewGuid(), "TypeScript") };
         modelBuilder.Entity<ProgrammingLanguage>().HasData(programmingLanguagesSeeds);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        var entities = ChangeTracker.Entries<Entity>();
+        foreach (var entity in entities)
+        {
+            _ = entity.State switch
+            {
+                EntityState.Added => entity.Entity.CreatedDate = DateTime.Now,
+                EntityState.Modified => entity.Entity.UpdatedDate = DateTime.Now,
+                EntityState.Deleted => entity.Entity.DeletedDate = DateTime.Now,
+                _ => DateTime.Now
+            };
+        }
+        return base.SaveChangesAsync(cancellationToken);
     }
 }
