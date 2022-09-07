f789d3154cb3ab52e0e809c2550677a1f276762d
haliltokszz <toksozhalil@gmail.com>
2022-09-08 00:04:59 +0300

    feature: DeletedProgrammingLanguageDTO.cs added.

---
+++ src/Project/Application/Features/ProgrammingLanguages/DTOs/DeletedProgrammingLanguageDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Features.ProgrammingLanguages.DTOs;
+
+public class DeletedProgrammingLanguageDTO
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime DeletedDate { get; set; }
+    public bool isDeleted { get; set; }
+}
