399ca37f424f927e35f98d6ac2068d7d99b676a8
haliltokszz <toksozhalil@gmail.com>
2022-09-07 15:04:41 +0300

    feature: DbContext added.

---
+++ src/Project/Persistence/Contexts/KodlamaIODevsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Core.Persistence.Dynamic;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Contexts;
+
+public class KodlamaIODevsDbContext : DbContext
+{
+    public KodlamaIODevsDbContext(DbContextOptions<KodlamaIODevsDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<ProgrammingLanguage> ProgrammingLanguages { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.AddGlobalFilter(); //GlobalFilter for getting only active records
+        base.OnModelCreating(modelBuilder);
+        
+        ProgrammingLanguage[] programmingLanguagesSeeds = { new(Guid.NewGuid(), "C#"), new(Guid.NewGuid(), "JavaScript"), new(Guid.NewGuid(), "TypeScript") };
+        modelBuilder.Entity<ProgrammingLanguage>().HasData(programmingLanguagesSeeds);
+    }
+}
