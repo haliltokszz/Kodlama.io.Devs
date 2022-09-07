e23f60d7180dc8d5256b6f58020a67a0d8654d05
haliltokszz <toksozhalil@gmail.com>
2022-09-08 00:04:46 +0300

    feature: UpdateProgrammingLanguageCommandValidator.cs added.

---
+++ src/Project/Application/Features/ProgrammingLanguages/Commands/UpdateCommand/UpdateProgrammingLanguageCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Application.Features.ProgrammingLanguages.Commands.UpdateCommand;
+
+public class UpdateProgrammingLanguageCommandValidator: AbstractValidator<UpdateProgrammingLanguageCommand>
+{
+    public UpdateProgrammingLanguageCommandValidator()
+    {
+        RuleFor(p=> p.Id)
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .NotNull();
+        RuleFor(p=> p.Name)
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .NotNull()
+            .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
+    }
+}
