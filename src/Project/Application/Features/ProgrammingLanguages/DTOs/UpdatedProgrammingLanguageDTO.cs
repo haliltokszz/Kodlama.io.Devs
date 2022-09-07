9961df8fe1a9fc055cc8b728f240acc07abb447a
haliltokszz <toksozhalil@gmail.com>
2022-09-08 00:05:11 +0300

    feature: UpdatedProgrammingLanguageDTO.cs added.

---
+++ src/Project/Application/Features/ProgrammingLanguages/DTOs/UpdatedProgrammingLanguageDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Features.ProgrammingLanguages.DTOs;
+
+public class UpdatedProgrammingLanguageDTO
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public bool isDeleted { get; set; }
+}
