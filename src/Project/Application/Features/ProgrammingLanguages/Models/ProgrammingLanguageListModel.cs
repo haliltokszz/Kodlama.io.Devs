44b59309b91f8b06f0e4459f0e3c2118388d36f0
haliltokszz <toksozhalil@gmail.com>
2022-09-07 15:11:10 +0300

    feature: ProgrammingLanguageListModel.cs added.

---
+++ src/Project/Application/Features/ProgrammingLanguages/Models/ProgrammingLanguageListModel.cs
@@ -0,0 +1,9 @@
+﻿using Application.Features.ProgrammingLanguages.DTOs;
+using Core.Persistence.Paging;
+
+namespace Application.Features.ProgrammingLanguages.Models;
+
+public class ProgrammingLanguageListModel : BasePageableModel 
+{
+    public IEnumerable<ProgrammingLanguageListDTO> Items { get; set; }
+}
