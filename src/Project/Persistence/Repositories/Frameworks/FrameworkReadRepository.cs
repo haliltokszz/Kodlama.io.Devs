f12acfe184c47c99204b3b0799538f92277948b7
haliltokszz <toksozhalil@gmail.com>
2022-09-15 00:59:29 +0300

    feature: Framework Read & Write repositories added.

---
+++ src/Project/Persistence/Repositories/Frameworks/FrameworkReadRepository.cs
@@ -0,0 +1,13 @@
+﻿using Application.Services.Repositories.Frameworks;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Persistence.Contexts;
+
+namespace Persistence.Repositories.Frameworks;
+
+public class FrameworkReadRepository: ReadRepository<Framework, KodlamaIODevsDbContext>, IFrameworkReadRepository
+{
+    public FrameworkReadRepository(KodlamaIODevsDbContext context) : base(context)
+    {
+    }
+}
---
+++ src/Project/Persistence/Repositories/Frameworks/FrameworkWriteRepository.cs
@@ -0,0 +1,13 @@
+﻿using Application.Services.Repositories.Frameworks;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Persistence.Contexts;
+
+namespace Persistence.Repositories.Frameworks;
+
+public class FrameworkWriteRepository: WriteRepository<Framework, KodlamaIODevsDbContext>, IFrameworkWriteRepository
+{
+    public FrameworkWriteRepository(KodlamaIODevsDbContext context) : base(context)
+    {
+    }
+}
