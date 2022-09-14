14ffaa5f388da75eee3db164c57e18917c3a0062
haliltokszz <toksozhalil@gmail.com>
2022-09-15 00:57:35 +0300

    feature: Frameworks relationship added to ProgrammingLanguage.cs

--- src/Project/Domain/Entities/ProgrammingLanguage.cs
+++ src/Project/Domain/Entities/ProgrammingLanguage.cs
@@ -5,6 +5,7 @@ namespace Domain.Entities;
 public class ProgrammingLanguage: Entity
 {
     public string Name { get; set; }
+    public virtual ICollection<Framework> Frameworks { get; set; }
 
     public ProgrammingLanguage()
     {
